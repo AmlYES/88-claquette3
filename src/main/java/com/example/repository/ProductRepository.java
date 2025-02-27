62b3f8054345ed78a7bff07322dc0d457ae3b2db
yasmineldiasty <yasmineldiasty3@gmail.com>
2025-02-27 19:10:45 +0200

    Product Service & Repo done

---
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -0,0 +1,77 @@
+package com.example.repository;
+
+import com.example.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Repository
+@SuppressWarnings("rawtypes")
+
+
+public class ProductRepository extends MainRepository<Product> {
+
+    @Override
+    protected String getDataPath() {
+        return "src/main/java/com/example/data/products.json";
+    }
+
+    @Override
+    protected Class<Product[]> getArrayType() { return Product[].class; }
+
+    //----- Required Methods -----//
+
+    public Product addProduct(Product product) {
+        save(product);
+        return product;
+    }
+
+    public ArrayList<Product> getProducts() { return findAll();}
+
+    public Product getProductById(UUID productId) {
+        ArrayList<Product> products = findAll();
+        for(Product product: products){
+            if(product.getId().equals(productId)){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product updateProduct(UUID productId, String newName, double newPrice) {
+        ArrayList<Product> products = findAll();
+        for (Product product : products) {
+            if (product.getId().equals(productId)) {
+                product.setName(newName);
+                product.setPrice(newPrice);
+                saveAll(products);
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void applyDiscount(double discount, ArrayList<UUID> productIds) {
+        ArrayList<Product> products = findAll();
+        for(Product product: products){
+            if(productIds.contains(product.getId())){
+                double discountedPrice = product.getPrice()- ( ( discount/100) * product.getPrice());
+                product.setPrice(discountedPrice);
+            }
+        }
+        saveAll(products);
+    }
+
+    public void deleteProductById(UUID productId) {
+        ArrayList<Product> products = findAll();
+        for(Product product: products){
+            if(productId.equals(product.getId())){
+                products.remove(product);
+            }
+        }
+        saveAll(products);
+    }
+}
---
+++ src/main/java/com/example/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.example.service;
+
+import com.example.model.Product;
+import com.example.repository.CartRepository;
+import com.example.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.service.MainService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Service
+@SuppressWarnings("rawtypes")
+public class ProductService extends MainService<Product> {
+    @Autowired
+    ProductRepository productRepository;
+
+    //----- Required Methods -----//
+
+    public Product addProduct(Product product) {return productRepository.addProduct(product);}
+
+    public ArrayList<Product> getProducts() {
+        return productRepository.getProducts();
+    }
+
+    public Product getProductById(UUID productId) {
+        return productRepository.getProductById(productId);
+    }
+
+    public Product updateProduct(UUID productId, String newName, double newPrice) {
+        return productRepository.updateProduct(productId, newName, newPrice);}
+
+    public void applyDiscount(double discount, ArrayList<UUID> productIds) {
+        productRepository.applyDiscount(discount, productIds);
+
+    }
+
+    public void deleteProductById(UUID productId) {
+        productRepository.deleteProductById(productId);
+    }
+
+
+}
